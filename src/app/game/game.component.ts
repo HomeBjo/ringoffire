1278952ee21d1b080b02a80fba8b89cca5d8018e
HomeBjo <122940319+HomeBjo@users.noreply.github.com>
2024-02-13 15:28:30 +0100

    add newGame

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
+
+
 
 @Component({
   selector: 'app-game',
@@ -10,8 +13,20 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  // game!: Game;
+  game: Game | undefined;
 
   constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game);
+
+  }
 
   takeCard(){
     this.pickCardAnimation = true;
