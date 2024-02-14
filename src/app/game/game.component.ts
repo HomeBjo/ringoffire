c41da533c99676aa5e0e55b24ff50ceea43e84a6
HomeBjo <122940319+HomeBjo@users.noreply.github.com>
2024-02-14 13:44:26 +0100

    add materials and icons

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,11 +2,15 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule,PlayerComponent],
+  imports: [CommonModule,PlayerComponent,MatSlideToggleModule,MatButtonModule,MatIconModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
